7b16e4b9813d33386e960af97bfba7af81afad8e
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-03 16:07:17 -0500

    created app page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cart from './components/Cart';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <>
+      <Menu />
+      <Cart />
+    </>
+  );
+}
+
+export default App;
